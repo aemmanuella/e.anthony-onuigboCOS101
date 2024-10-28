5f1d35b07e29fdb78e32bb20a59e5e76bd873b5a
aemmanuella <emmanuella.anthony-onuigbo@pau.edu.ng>
2024-10-28 20:07:37 +0100

    (Week 3 commit)

---
+++ week-3/practice_5/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let special_character = '@'; //default
+    let alphabet:char ='B';
+    let surname = "Anthony-Onuigbo";
+    let first_name = "Emmanuella";
+    let middle_name = "Onyinyechi";
+
+    println!(" ");
+    println!("Special Character: {}", special_character);
+    println!("Alphabet: {}", alphabet);
+    println!("Name: {} {} {}", surname, middle_name, first_name);
+
+}
---
+++ week-3/practice_9/target/debug/practice_9.d
@@ -0,0 +1,1 @@
+C:\Users\PC\Documents\e.anthony-onuigboCOS101\week-3\practice_9\target\debug\practice_9.exe: C:\Users\PC\Documents\e.anthony-onuigboCOS101\week-3\practice_9\src\main.rs
